a48216c388226377dedd75f3112f14b67cddd8a9
Sergio Gutiérrez <Serchinastico@users.noreply.github.com>
2018-09-05 10:38:57 +0200

    Update InferenceNameTest.kt

--- core/src/test/kotlin/com/karumi/kotlinsnapshot/core/InferenceNameTest.kt
+++ core/src/test/kotlin/com/karumi/kotlinsnapshot/core/InferenceNameTest.kt
@@ -35,7 +35,7 @@ class InferenceNameNotSupported {
     private val snap = Camera(KotlinSerialization(), TestCaseExtractorNotSupported())
 
     @Test(expected = TestNameNotFoundException::class)
-    fun if_the_test_name_can_not_be_found_and_exception_will_be_thrown() {
+    fun if_the_test_name_can_not_be_found_an_exception_will_be_thrown() {
         val fran = Developer("Fran", 1)
         snap.matchWithSnapshot(fran)
     }
